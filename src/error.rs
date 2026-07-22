ea847ad180f59463561fe73b728c48b59742c636
nanashi-1 <bagaresalken@gmail.com>
2026-07-22 22:07:17 +0800

    Add documentation to errors

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
+/// Errors that can occur during grid operations.
 #[derive(thiserror::Error, Debug, Clone, PartialEq)]
 pub enum GridError {
+    /// Attempted to access a grid coordinate that is out of bounds.
     #[error("Grid index out of bounds: tried to access coordinate ({x}, {y}) on a grid with dimensions ({width}, {height})")]
     OutOfBounds {
         x: usize,
